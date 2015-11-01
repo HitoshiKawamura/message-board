9f1df80b4e0599d635fcae8bf0982d7bb17a2ee1
hitoshikawamura <h.kawamura0316@gmail.com>
2015-11-01 12:17:45 +0000

    delete botton

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,25 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
   def index
     # Messageをすべて取得する
     @messages = Message.all
     @message = Message.new
+  end
+  def edit
+  end
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
   end
   
   ## ここから追記
@@ -25,5 +42,8 @@ class MessagesController < ApplicationController
     # 返り値は ex:) {name: "入力されたname" , body: "入力されたbody" }
     params.require(:message).permit(:name, :body)
   end
+  def set_message
+    @message = Message.find(params[:id])
+  end  
   ## ここまで
 end
